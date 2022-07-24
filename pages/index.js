dc8db2941697c514c468893b41f0fb2c2c05acb7
marklin2289 <marklin2289@gmail.com>
2022-07-24 01:12:21 -0700

    yarn add react react-dom moralis react-moralis

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
+import Header from ".././components/Header"
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <h1>Hello ! Testing connection. </h1>
+            <Header />
         </div>
     )
 }
