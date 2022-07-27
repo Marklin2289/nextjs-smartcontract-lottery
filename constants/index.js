78a5bead078faee3299f3d2579963752bff36dc1
marklin2289 <marklin2289@gmail.com>
2022-07-26 22:15:45 -0700

    created index.js to exports abi and contractaddress.json

---
+++ constants/index.js
@@ -0,0 +1,9 @@
+// exports abi and contractAddresses.json
+
+const contractAddresses = require("./contractAddresses.json")
+const abi = require("./abi.json")
+
+module.exports = {
+    abi,
+    contractAddresses,
+}
