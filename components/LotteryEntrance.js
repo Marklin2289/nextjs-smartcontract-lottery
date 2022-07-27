752b6d4d2dc515d0ac504f64c4be814f4d4d3fe1
marklin2289 <marklin2289@gmail.com>
2022-07-26 22:11:51 -0700

    added local network and build constants ABI and contractAddress json

---
+++ components/LotteryEntrance.js
@@ -0,0 +1,14 @@
+// have a function to enter the Lottery
+import { useWeb3Contract } from "react-moralis"
+
+export default function LotteryEntrance() {
+    // const {runContractFunction: enterRaffle} = useWeb3Contract({
+    //     abi: ,// ,
+    //     contractAddress: ,//,
+    //     functionName: ,//,
+    //     params: {},
+    //     msgValue: ,//
+    // })
+
+    return <div>Hi from LotteryEntrance</div>
+}
